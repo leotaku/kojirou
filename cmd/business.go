65e965df8457ca8ed54d57294256c88f516e2189
Leo Gaskin <leo.gaskin@le0.gs>
2021-12-11 19:54:54 +0100

    Fix download logic
    Closes #3
    
    Co-authored-by: Colin1224 <colin.cope@gmail.com>

--- cmd/business.go
+++ cmd/business.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"image/jpeg"
 	"os"
+	"path"
 
 	"github.com/cheggaaa/pb/v3"
 	"github.com/hashicorp/go-retryablehttp"
@@ -11,57 +13,136 @@ import (
 )
 
 const (
-	progressTemplate = `{{ string . "prefix" | printf "%-10v" }} {{ bar . "|" "█" "▌" " " "|" }} {{ counters . | printf "%-15v" }} {{ "|" }}`
+	progressTemplate = `` +
+		`{{ string . "prefix" | printf "%-10v" }}` +
+		`{{ bar . "|" "█" "▌" " " "|" }}` + `{{ " " }}` +
+		`{{ if string . "message" }}` +
+		`{{   string . "message" | printf "%-15v" }}` +
+		`{{ else }}` +
+		`{{   counters . | printf "%-15v" }}` +
+		`{{ end }}` + `{{ " |" }}`
 )
 
 func runBusinessLogic(mangaID string) error {
-	// Create API client
 	retry := retryablehttp.NewClient()
 	retry.Logger = nil
 	http := retry.StandardClient()
 	client := md.NewClient().WithHTTPClient(http)
 
-	manga, err := downloadManga(client, mangaID)
+	manga, err := businessDownloadManga(client, mangaID)
+	if err != nil {
+		return fmt.Errorf("download: %w", err)
+	}
+	printMangaSummary(*manga)
+	if dryRunArg {
+		return nil
+	}
+
+	bar := pb.New(0).SetTemplate(progressTemplate)
+	bar.Set("prefix", fmt.Sprintf("Covers"))
+	bar.Set(pb.CleanOnFinish, true)
+	bar.Start()
+	dl := formats.NewMangadexDownloader(client, http, progress(bar))
+	covers, err := formats.MangadexCovers(dl, manga.Info.ID)
 	if err != nil {
 		return err
 	}
-	printMangaSummary(*manga)
+	*manga = manga.WithCovers(covers)
+	bar.Finish()
 
-	if dryRunArg {
-		return nil
+	bookDirectory := "."
+	thumbnailDirectory := new(string)
+	switch {
+	case kindleFolderModeArg && outArg != "":
+		bookDirectory = path.Join(outArg, "documents", manga.Info.Title)
+		*thumbnailDirectory = path.Join(outArg, "system", "thumbnails")
+	case kindleFolderModeArg:
+		bookDirectory = path.Join("kindle", "documents", manga.Info.Title)
+		*thumbnailDirectory = path.Join("kindle", "system", "thumbnails")
+	case outArg != "":
+		bookDirectory = outArg
+	default:
+		bookDirectory = manga.Info.Title
 	}
 
 	for _, volume := range manga.Sorted() {
 		bar := pb.New(0).SetTemplate(progressTemplate)
 		bar.Set("prefix", fmt.Sprintf("Volume: %v", volume.Info.Identifier))
-		dl := formats.NewMangadexDownloader(client, http, progress(bar))
-
 		bar.Start()
-		chapters := volume.Sorted()
-		pages, err := formats.MangadexPages(dl, chapters)
-		if err != nil {
-			return err
-		}
-		bar.Finish()
 
-		bar = pb.New(0).SetTemplate(progressTemplate)
-		bar.Set("prefix", "Writing...")
-		bar.Set(pb.CleanOnFinish, true)
+		bookFilename := path.Join(bookDirectory, volume.Info.Identifier.StringFilled(4, 2, false)+".azw3")
+		switch _, err := os.Stat(bookFilename); {
+		case !(os.IsNotExist(err) || forceArg):
+			bar.SetTotal(1).SetCurrent(1)
+			bar.Set("message", "Skipped")
+			bar.Finish()
+		default:
+			dl := formats.NewMangadexDownloader(client, http, progress(bar))
+			chapters := volume.Sorted()
+			pages, err := formats.MangadexPages(dl, chapters)
+			bar.Finish()
 
-		bar.Start()
-		f, err := os.Create(volume.Info.Identifier.StringFilled(4, 2, false) + ".azw3")
-		if err != nil {
-			return fmt.Errorf("create volume %s: %w", volume.Info.Identifier, err)
+			if err != nil {
+				return fmt.Errorf("download: %w", err)
+			} else if err := businessWriteBook(
+				manga.WithChapters(chapters).WithPages(pages),
+				bookFilename,
+				thumbnailDirectory,
+			); err != nil {
+				return fmt.Errorf("write: %w", err)
+			}
 		}
-		manga := manga.WithChapters(chapters).WithPages(pages)
-		if err := formats.WriteMOBI(manga).Realize().Write(f); err != nil {
-			return fmt.Errorf("write volume %s: %w", volume.Info.Identifier, err)
-		}
-
-		bar.Finish()
 	}
 
 	return nil
+}
+
+func businessWriteBook(manga md.Manga, bookFilename string, thumbnailDirectory *string) error {
+	mobi := formats.WriteMOBI(manga)
+	bar := pb.New(0).SetTemplate(progressTemplate)
+	bar.Set("prefix", "Writing...")
+	bar.Set(pb.CleanOnFinish, true)
+	bar.Start()
+	defer bar.Finish()
+
+	if err := os.MkdirAll(path.Dir(bookFilename), os.ModePerm); err != nil {
+		return err
+	} else if f, err := os.Create(bookFilename); err != nil {
+		return err
+	} else if err := mobi.Realize().Write(bar.NewProxyWriter(f)); err != nil {
+		return err
+	}
+
+	if thumbnailDirectory == nil || mobi.CoverImage == nil {
+		return nil
+	} else if err := os.MkdirAll(*thumbnailDirectory, os.ModePerm); err != nil {
+		return err
+	} else if t, err := os.Create(path.Join(*thumbnailDirectory, mobi.GetThumbFilename())); err != nil {
+		return err
+	} else if err := jpeg.Encode(t, mobi.CoverImage, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func businessDownloadManga(client *md.Client, mangaID string) (*md.Manga, error) {
+	manga, err := client.FetchManga(mangaID)
+	if err != nil {
+		return nil, fmt.Errorf("manga: %w", err)
+	}
+	chapters, err := client.FetchChapters(mangaID)
+	if err != nil {
+		return nil, fmt.Errorf("chapters: %w", err)
+	}
+
+	chapters, err = filterFromFlags(chapters)
+	if err != nil {
+		return nil, fmt.Errorf("filter: %w", err)
+	}
+
+	result := manga.WithChapters(chapters)
+	return &result, nil
 }
 
 func progress(bar *pb.ProgressBar) formats.Reporter {
@@ -72,25 +153,4 @@ func progress(bar *pb.ProgressBar) formats.Reporter {
 			bar.Add(-n)
 		}
 	}
-}
-
-func downloadManga(client *md.Client, mangaID string) (*md.Manga, error) {
-	// Fetch manga and chapter data
-	manga, err := client.FetchManga(mangaID)
-	if err != nil {
-		return nil, fmt.Errorf("download manga: %w", err)
-	}
-	chapters, err := client.FetchChapters(mangaID)
-	if err != nil {
-		return nil, fmt.Errorf("download chapters: %w", err)
-	}
-
-	// Filter chapters
-	chapters, err = filterFromFlags(chapters)
-	if err != nil {
-		return nil, fmt.Errorf("filter: %w", err)
-	}
-
-	result := manga.WithChapters(chapters)
-	return &result, nil
 }
